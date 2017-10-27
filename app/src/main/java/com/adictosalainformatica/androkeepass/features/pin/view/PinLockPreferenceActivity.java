c25ebf0bf2a625ef70db658636b4688112682aaa
ruben miguel corcoba <ruben.feina@gmail.com>
2017-10-27 19:26:51 +0200

    minor changes

--- app/src/main/java/com/adictosalainformatica/androkeepass/features/pin/view/PinLockPreferenceActivity.java
+++ app/src/main/java/com/adictosalainformatica/androkeepass/features/pin/view/PinLockPreferenceActivity.java
@@ -7,7 +7,6 @@ import android.preference.SwitchPreference;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -55,26 +54,6 @@ public class PinLockPreferenceActivity extends AppCompatActivity{
                     .add(android.R.id.content, pinLockPreferenceFragment, KEY_PREFERENCE_FRAGMENT)
                     .commit();
         }
-
-        /*setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-
-        ActionBar actionBar = getSupportActionBar();
-
-        if (actionBar != null) {
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }*/
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
