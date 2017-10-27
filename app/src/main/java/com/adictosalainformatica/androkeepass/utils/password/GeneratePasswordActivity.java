c68e44c95669c4ef3d0ac4c8ca5e5d3b8457c7b1
ruben miguel corcoba <ruben.feina@gmail.com>
2017-10-27 19:38:53 +0200

    improve butterknife

--- app/src/main/java/com/adictosalainformatica/androkeepass/utils/password/GeneratePasswordActivity.java
+++ app/src/main/java/com/adictosalainformatica/androkeepass/utils/password/GeneratePasswordActivity.java
@@ -1,44 +1,64 @@
-package com.adictosalainformatica.androkeepass.features.loadfile.presentation.view.activity;
+/*
+ *      DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                  Version 2, December 2004
+ *
+ *      Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *
+ *      Everyone is permitted to copy and distribute verbatim or modified
+ *      copies of this license document, and changing it is allowed as long
+ *      as the name is changed.
+ *
+ *      DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *      TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *      0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+package com.adictosalainformatica.androkeepass.utils.password;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.adictosalainformatica.androkeepass.R;
-import com.adictosalainformatica.androkeepass.utils.PasswordGenerator;
+
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import timber.log.Timber;
 
 public class GeneratePasswordActivity extends AppCompatActivity {
 
-    @BindView(R.id.generate_password_button)
-    Button generatePasswordButton;
-    @BindView(R.id.generate_password_check_box_lower_case)
-    CheckBox lowerCaseCheckBox;
-    @BindView(R.id.generate_password_check_box_upper_case)
-    CheckBox upperCaseCheckBox;
-    @BindView(R.id.generate_password_check_box_numbers)
-    CheckBox numbersCheckBox;
-    @BindView(R.id.generate_password_check_box_punctuation)
-    CheckBox punctuationCheckBox;
     @BindView(R.id.generate_password_length)
     SeekBar passwordLengthProgressBar;
     @BindView(R.id.generatepassword_generated_password)
     TextView generatedPasswordTextView;
+
+    @BindViews({
+            R.id.generate_password_check_box_lower_case,
+            R.id.generate_password_check_box_upper_case,
+            R.id.generate_password_check_box_numbers,
+            R.id.generate_password_check_box_punctuation})
+    List<CheckBox> passwordOptionsList;
+
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_password);
         ButterKnife.bind(this);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         passwordLengthProgressBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -57,8 +77,6 @@ public class GeneratePasswordActivity extends AppCompatActivity {
             }
         });
     }
-
-
 
     @OnClick(R.id.generate_password_check_box_lower_case)
     public void onLowerCaseCheckBox(){
@@ -82,9 +100,6 @@ public class GeneratePasswordActivity extends AppCompatActivity {
 
     @OnClick(R.id.generate_password_button)
     public void onViewClicked(){
-        String password;
-        password = generatedPasswordTextView.getText().toString();
-
         if(!password.isEmpty()){
             Timber.d("password: " + password);
 
@@ -93,16 +108,15 @@ public class GeneratePasswordActivity extends AppCompatActivity {
             setResult(Activity.RESULT_OK, resultIntent);
             finish();
         }
-
     }
 
     private void generatePassword(){
-        String password = new PasswordGenerator
+        password = new PasswordGenerator
                 .PasswordGeneratorBuilder()
-                .useDigits(numbersCheckBox.isChecked())
-                .useLower(lowerCaseCheckBox.isChecked())
-                .useUpper(upperCaseCheckBox.isChecked())
-                .usePunctuation(punctuationCheckBox.isChecked())
+                .useLower(passwordOptionsList.get(0).isChecked())
+                .useUpper(passwordOptionsList.get(1).isChecked())
+                .useDigits(passwordOptionsList.get(2).isChecked())
+                .usePunctuation(passwordOptionsList.get(3).isChecked())
                 .build().generate(passwordLengthProgressBar.getProgress() + 1);
 
         generatedPasswordTextView.setText(getString(R.string.generatedpassword_generated_password)
