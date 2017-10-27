2955ab59bd276b6157acf5fcca48c7b3cfd99e29
ruben miguel corcoba <ruben.feina@gmail.com>
2017-10-27 19:27:17 +0200

    add dagger scope

---
+++ app/src/main/java/com/adictosalainformatica/androkeepass/base/dagger/scopes/PerActivity.java
@@ -0,0 +1,31 @@
+package com.adictosalainformatica.androkeepass.base.dagger.scopes;
+/*
+ *      DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                  Version 2, December 2004
+ *
+ *      Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *
+ *      Everyone is permitted to copy and distribute verbatim or modified
+ *      copies of this license document, and changing it is allowed as long
+ *      as the name is changed.
+ *
+ *      DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *      TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *      0. You just DO WHAT THE FUCK YOU WANT TO.
+ */
+
+import java.lang.annotation.Retention;
+
+import javax.inject.Scope;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * A scoping annotation to permit objects whose lifetime should
+ * conform to the life of the activity to be memorized in the
+ * correct component.
+ */
+@Scope
+@Retention(RUNTIME)
+public @interface PerActivity {}
